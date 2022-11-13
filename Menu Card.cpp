56924e417479c158a6e48fb3ffd53b155d6b4b84
Ashish Chachane <117847834+aashishchachane@users.noreply.github.com>
2022-11-14 00:30:57 +0530

    Add files via upload

---
+++ Menu Card.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int ch;
+	char val;
+	cout<<"Menu Card : "<<endl;
+	cout<<"1. Coffee "<<endl<<"2. Tea "<<endl;
+	cout<<" What would you like to have : ";
+	cin>>ch;
+	
+	do
+	{
+		if(ch==1)
+		{
+			cout<<"Enjoy your Coffee !!!";
+		}
+		else if(ch==2)
+		{
+			cout<<"Enjoy your Tea !!!";
+		}
+		else
+		{
+			cout<<"This is not in our Menu card !!!"<<endl;
+		}
+		
+		cout<<endl<<endl;
+		cout<<"Do you want to continue  ?";
+		cin>>val;
+		
+	}while(val=='y');
+
+
+
+	return 0;
+
+
+}
