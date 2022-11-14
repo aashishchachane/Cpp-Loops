9054cad8f4e0f603d8282f06bd7240113b160a41
Ashish Chachane <117847834+aashishchachane@users.noreply.github.com>
2022-11-14 15:03:23 +0530

    Add files via upload

---
+++ Pattern printing.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int i,n;
+	cout<<"How many stars do you wanna print : ";
+	cin>>n;
+	for(i=1;i<=n;i++)
+	{
+		cout<<"*";
+	}
+	
+	return 0;
+}
